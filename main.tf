e674ac53f73bf7f57752248290cc449843626383
Keith Reinhardt <reinhake@oregonstate.edu>
2025-06-09 15:49:54 -0700

    Updated the script to be ran from a provisioner instead of user_data

--- main.tf
+++ main.tf
@@ -1,57 +1,84 @@
 # main.tf
 
 terraform {
-  required_providers {
+required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+        source  = "hashicorp/aws"
+        version = "~> 4.16"
     }
-  }
+}
 
-  required_version = ">= 1.2.0"
+    required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region = "us-west-2"
+    region = "us-west-2"
 }
 
 resource "aws_instance" "minecraft" {
-  ami           = "ami-0e63f5cb6f14d1dcf"
-  instance_type = "t3.small"
+    ami           = "ami-0e63f5cb6f14d1dcf"
+    instance_type = "t3.small"
 
-  key_name = "minecraft"
+    key_name = "minecraft"
 
-  vpc_security_group_ids = [aws_security_group.minecraft_sg.id]
+    vpc_security_group_ids = [aws_security_group.minecraft_sg.id]
 
-  user_data = file("startup.sh")
+    
+    provisioner "file" {
+        source = "startup.sh"
+        destination = "/home/ec2-user/startup.sh"
 
-  tags = {
-    Name = "MinecraftServer"
-  }
+        connection {
+            type = "ssh"
+            user = "ec2-user"
+            private_key = file("minecraft.pem")
+            host = self.public_ip
+        }
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /home/ec2-user/startup.sh",
+            "sudo /home/ec2-user/startup.sh"
+        ]
+
+        connection {
+            type = "ssh"
+            user = "ec2-user"
+            private_key = file("minecraft.pem")
+            host = self.public_ip
+        }
+    }
+    
+    tags = {
+        Name = "MinecraftServer"
+    }
 }
 
+
+
 resource "aws_security_group" "minecraft_sg" {
-  name        = "minecraft_sg"
-  description = "Allow for connections to the server"
+    name        = "minecraft_sg"
+    description = "Allow for connections to the server"
 
-  ingress {
-    from_port  = 25565
-    to_port    = 25565
-    protocol   = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    ingress {
+        from_port  = 25565
+        to_port    = 25565
+        protocol   = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  ingress {
-    from_port  = 22
-    to_port    = 22
-    protocol   = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    ingress {
+        from_port  = 22
+        to_port    = 22
+        protocol   = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
